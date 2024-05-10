02ee8ffed3b2e8e10b8a38cd873810bb54f41495
Erick Santiago <131812522+ErickSantiago0013@users.noreply.github.com>
2024-05-10 16:26:32 -0300

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+ 
+rua. escrever ( """
+# Meu primeiro aplicativo
+Olá Mundo!*
+""" )
+ 
+df = pd. read_csv ( "meus_dados.csv" )
+rua. gráfico_de_linha ( df )
