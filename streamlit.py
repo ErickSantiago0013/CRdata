7953ab6c8dce053a43e28b64ddae9d84543377fa
Erick Santiago <131812522+ErickSantiago0013@users.noreply.github.com>
2024-05-10 17:00:36 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,40 +1,11 @@
 import streamlit as st
-from datetime import time, datetime
+import pandas as pd
+import numpy as np
 
-st.header('st.slider')
+st.header('Gráfico de linhas')
 
-# Exemplo 1
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
 
-st.subheader('Slider')
-
-age = st.slider('Quantos anos você tem?', 0, 130, 25)
-st.write("Eu tenho ", age, ' anos')
-
-
-# Exemplo 2
-
-st.subheader('Slider de intervalo')
-
-values = st.slider(
-     'Escolha um intervalo de valores',
-     0.0, 100.0, (25.0, 75.0))
-st.write('Valores:', values)
-
-# Exemplo 3
-
-st.subheader('Slider de intervalo de tempo')
-
-appointment = st.slider(
-     "Agende um compromisso:",
-     value=(time(11, 30), time(12, 45)))
-st.write("O compromisso foi agendado para:", appointment)
-
-# Exemplo 4
-
-st.subheader('Slider de data e hora')
-
-start_time = st.slider(
-     "Quando você vai começar?",
-     value=datetime(2020, 1, 1, 9, 30),
-     format="MM/DD/YY - hh:mm")
-st.write("Início:", start_time)
+st.line_chart(chart_data)
