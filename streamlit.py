b67f1d7a70199e7b2ca4b81385292db75280d5db
Erick Santiago <131812522+ErickSantiago0013@users.noreply.github.com>
2024-05-10 17:37:57 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,15 +1,44 @@
 import streamlit as st
-import time
 
-st.title('st.progress')
+st.title('st.form')
 
-with st.expander('Sobre esta aplicação'):
-     st.write('Agora você pode exibir o progresso od seus calculosthe progress of your e, uma aplicação Streamlit com o comando `st.progress`.')
+# Exemplo completo usando a notação with
+st.header('1. Exemplo usando a notação `with`')
+st.subheader('Cafeteira')
 
-my_bar = st.progress(0)
+with st.form('my_form'):
+    st.subheader('**Escolha seu café**')
 
-for percent_complete in range(100):
-     time.sleep(0.05)
-     my_bar.progress(percent_complete + 1)
+    # Componentes de entrada
+    coffee_bean_val = st.selectbox('Grão', ['Arabica', 'Robusta'])
+    coffee_roast_val = st.selectbox('Torra', ['Clara', 'Média', 'Escura'])
+    brewing_val = st.selectbox('Método', ['Aeropress', 'Filtrado', 'Prensa Francesa', 'Cafeteira Italiana', 'Globo'])
+    serving_type_val = st.selectbox('Formato', ['Quente', 'Gelado', 'Frapê'])
+    milk_val = st.select_slider('Leite', ['Não', 'Pouco', 'Médio', 'Muito'])
+    owncup_val = st.checkbox('Trouxe o meu copo!')
 
-st.balloons()
+    # Todo formulário deve ter um botão enviar
+    submitted = st.form_submit_button('Enviar')
+
+if submitted:
+    st.markdown(f'''
+        ☕ Você pediu:
+        - Grão: `{coffee_bean_val}`
+        - Torra: `{coffee_roast_val}`
+        - Método: `{brewing_val}`
+        - Formato: `{serving_type_val}`
+        - Leite: `{milk_val}`
+        - Trouxe o meu copo: `{owncup_val}`
+        ''')
+else:
+    st.write('☝️ Faça o seu pedido!')
+
+
+# Pequeno exemplo usando objeto
+st.header('2. Exemplo com objeto')
+
+form = st.form('my_form_2')
+selected_val = form.slider('Escolha um valor')
+form.form_submit_button('Enviar')
+
+st.write('Valor escolhido: ', selected_val)
