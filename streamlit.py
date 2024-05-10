fae219e9b162c9855d0a32aa58fc990a1b91b7b0
Erick Santiago <131812522+ErickSantiago0013@users.noreply.github.com>
2024-05-10 17:30:22 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,17 +1,5 @@
 import streamlit as st
 
-st.title('Customizando o tema de aplicações Streamlit')
+st.title('st.secrets')
 
-st.write('Conteúdo do arquivo `.streamlit/config.toml` desta aplicação')
-
-st.code("""
-[theme]
-primaryColor="#F39C12"
-backgroundColor="#2E86C1"
-secondaryBackgroundColor="#AED6F1"
-textColor="#FFFFFF"
-font="monospace"
-""")
-
-number = st.sidebar.slider('Selecione um número:', 0, 10, 5)
-st.write('O número selecionado no controle deslizante é:', number)
+st.write(st.secrets['message'])
