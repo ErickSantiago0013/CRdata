a9ec6f1ac0130c1b82fd355a02dd266da78f7cb2
Erick Santiago <131812522+ErickSantiago0013@users.noreply.github.com>
2024-05-10 17:39:48 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,44 +1,30 @@
 import streamlit as st
 
-st.title('st.form')
+st.title('st.experimental_get_query_params')
 
-# Exemplo completo usando a notação with
-st.header('1. Exemplo usando a notação `with`')
-st.subheader('Cafeteira')
+with st.expander('Sobre esta aplicação'):
+  st.write("`st.experimental_get_query_params` recupera parâmetros da consulta(query paramaters) diretamente da URL do navegador.")
 
-with st.form('my_form'):
-    st.subheader('**Escolha seu café**')
-
-    # Componentes de entrada
-    coffee_bean_val = st.selectbox('Grão', ['Arabica', 'Robusta'])
-    coffee_roast_val = st.selectbox('Torra', ['Clara', 'Média', 'Escura'])
-    brewing_val = st.selectbox('Método', ['Aeropress', 'Filtrado', 'Prensa Francesa', 'Cafeteira Italiana', 'Globo'])
-    serving_type_val = st.selectbox('Formato', ['Quente', 'Gelado', 'Frapê'])
-    milk_val = st.select_slider('Leite', ['Não', 'Pouco', 'Médio', 'Muito'])
-    owncup_val = st.checkbox('Trouxe o meu copo!')
-
-    # Todo formulário deve ter um botão enviar
-    submitted = st.form_submit_button('Enviar')
-
-if submitted:
-    st.markdown(f'''
-        ☕ Você pediu:
-        - Grão: `{coffee_bean_val}`
-        - Torra: `{coffee_roast_val}`
-        - Método: `{brewing_val}`
-        - Formato: `{serving_type_val}`
-        - Leite: `{milk_val}`
-        - Trouxe o meu copo: `{owncup_val}`
-        ''')
-else:
-    st.write('☝️ Faça o seu pedido!')
+# 1. Instruções
+st.header('1. Instruções')
+st.markdown('''
+Na barra de URL do seu navegador de internet, anexe o seguinte:
+`?name=Jack&surname=Beanstalk`
+depois da URL base `http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/`
+então ficará assim 
+`http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/?firstname=Jack&surname=Beanstalk`
+''')
 
 
-# Pequeno exemplo usando objeto
-st.header('2. Exemplo com objeto')
+# 2. Conteúdos do st.experimental_get_query_params
+st.header('2. Conteúdos do st.experimental_get_query_params')
+st.write(st.experimental_get_query_params())
 
-form = st.form('my_form_2')
-selected_val = form.slider('Escolha um valor')
-form.form_submit_button('Enviar')
 
-st.write('Valor escolhido: ', selected_val)
+# 3. Recuperando e exibindo informações da URL
+st.header('3. Recuperando e exibindo informações da URL')
+
+firstname = st.experimental_get_query_params()['firstname'][0]
+surname = st.experimental_get_query_params()['surname'][0]
+
+st.write(f'Olá **{firstname} {surname}**, tudo bem?')
