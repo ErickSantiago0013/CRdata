4c25dbae34f4b082d9b76afa12ee2c717b21486b
Erick Santiago <131812522+ErickSantiago0013@users.noreply.github.com>
2024-05-10 17:21:07 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,40 +1,11 @@
 import streamlit as st
 import pandas as pd
-import time
-import random
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
 
-# Função para gerar dados de vendas simuladas
-def generate_sales_data():
-    while True:
-        # Simula dados de venda aleatórios
-        product_id = random.randint(1, 100)
-        quantity = random.randint(1, 10)
-        price = round(random.uniform(10.0, 100.0), 2)
-        
-        # Retorna os dados de venda simulados como um dicionário
-        yield {"Product ID": product_id, "Quantity": quantity, "Price": price}
-        
-        # Aguarda um intervalo antes de gerar o próximo conjunto de dados
-        time.sleep(1)
+st.header('`streamlit_pandas_profiling`')
 
-# Criar uma página da web com o Streamlit
-def main():
-    st.title("Dados de Vendas Simuladas")
-    
-    # Criar uma lista vazia para armazenar os dados de vendas
-    sales_data = []
-    
-    # Criar um componente para exibir a tabela na página da web
-    table = st.table(pd.DataFrame())
-    
-    # Iniciar o streaming de dados de vendas simuladas
-    for sale in generate_sales_data():
-        # Adicionar a venda à lista de dados de vendas
-        sales_data.append(sale)
-        
-        # Atualizar o DataFrame e exibir a tabela na página da web
-        table.dataframe(pd.DataFrame(sales_data))
-        
-# Executar o aplicativo principal
-if __name__ == "__main__":
-    main()
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+pr = df.profile_report()
+st_profile_report(pr)
