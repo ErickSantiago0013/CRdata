482cf8d05e981f9528a771b5e3be4f7638c767bb
Erick Santiago <131812522+ErickSantiago0013@users.noreply.github.com>
2024-05-10 17:31:20 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import pandas as pd
 
-st.title('st.secrets')
+st.title('st.file_uploader')
 
-st.write(st.secrets['message'])
+st.subheader('Upload de CSV')
+uploaded_file = st.file_uploader("Escolha um arquivo")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Estatístiscas descritivas')
+  st.write(df.describe())
+else:
+  st.info('☝️ Faça upload de um arquivo CSV')
